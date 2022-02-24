df74c8006a37e58fa34c0ca966c26bf4485f74c5
Jared Grippe <jared@deadlyicon.com>
2022-02-24 11:45:54 -0800

    WIP

--- app/index.js
+++ app/index.js
@@ -24,6 +24,7 @@ function createApp({ appName, appColor, port }){
   // ROUTES
   app.use('*', (req, res, next) => {
     // res.locals.currentUser = getCurrentUser(req)
+    console.log('COOKIES', req.cookies)
     res.locals.currentUser = {
       did: 'did:jlinc:8AZLka1zkZ8Ve5bK_mS9QwS9oTkTX4IgwhPR4FW99iQ',
       email: 'frog@example.com',
@@ -33,6 +34,10 @@ function createApp({ appName, appColor, port }){
 
   app.get('/', (req, res) => {
     res.render('index');
+  })
+
+  app.get('/logout', (req, res) => {
+    res.send('/login TBD')
   })
 
   app.get('/login', (req, res) => {

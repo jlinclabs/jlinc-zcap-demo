7d16c882eb166904ed81607bbb54989b4a1dd7e9
Jared Grippe <jared@deadlyicon.com>
2022-03-29 13:58:15 -0700

    WIP

---
+++ app/postgresql.js
@@ -0,0 +1,40 @@
+const { Pool: PGPool } = require('pg')
+const { readFile } = require('fs').promises
+const Path = require('path')
+
+const SCHEMA_PATH = Path.resolve(__dirname, './schema.sql')
+
+module.exports = function(options){
+  const pg = new PGPool({
+    connectionString: options.postgresDatabaseUrl,
+  })
+  pg.resetSchema = resetSchema;
+  return pg
+}
+
+async function resetSchema(){
+  const schema = await readFile(SCHEMA_PATH)
+  await this.query(`${schema}`)
+}
+
+// class Postgresql {
+//   constructor(connectionString){
+//     this.pg = new PGPool({ connectionString })
+//   }
+
+//   async reset(){
+//     const schema = await readFile(SCHEMA_PATH)
+//     await this.pg.query(`${schema}`)
+//   }
+
+//   async query(...args){
+//     const results = await this.pg.query(...args)
+//     return results.rows
+//   }
+
+//   async one(...args){
+//     const [ row ] = await this.query(...args)
+//     return row
+//   }
+// }
+

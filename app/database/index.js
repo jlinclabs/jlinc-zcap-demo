8eb24a498063582e1a3577a76271aac890215ee2
Jared Grippe <jared@deadlyicon.com>
2022-03-29 12:29:46 -0700

    WIP

--- app/database/index.js
+++ app/database/index.js
@@ -16,14 +16,17 @@ module.exports = class Database {
 
   async getAllUsers(){
     const { rows: users } = await this.pg.query('SELECT * FROM users')
+    // get user profiles from hypercores
     return users
   }
 
   async getUser(username){
-    const { rows: [ user ] } = await this.pg.query(
-      `SELECT * FROM users WHERE username = $1`,
-      [username],
-    )
+    const users = await this.getAllUsers()
+    return users.find(user => user.username === username)
+    // const { rows: [ user ] } = await this.pg.query(
+    //   `SELECT * FROM users WHERE username = $1`,
+    //   [username],
+    // )
     return user
   }
 

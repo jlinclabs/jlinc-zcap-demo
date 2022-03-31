9090b15f349ad895aa8c1270442faf726a59a699
Jared Grippe <jared@deadlyicon.com>
2022-03-31 11:44:20 -0700

    WIP

---
+++ bootstrapper.js
@@ -0,0 +1,27 @@
+const DHT = require('@hyperswarm/dht')
+
+const bootstrapper = DHT.bootstrapper(49736, {
+  ephemeral: true,
+  seed: DHT.hash(Buffer.from('jlinc-zcap-demo-dht-bootstrap')),
+})
+
+bootstrapper.ready().then(() => {
+  console.log(
+    'Hyperswarm bootstrapper running on port',
+    bootstrapper.address().port
+  )
+})
+
+bootstrapper.on('add-node', node => {
+  console.log('node added', node.id.toString('hex'))
+})
+
+bootstrapper.on('remove-node', node => {
+  console.log('node removed', node.id.toString('hex'))
+})
+
+process.once('SIGINT', async function () {
+  console.log('Closing server...')
+  await bootstrapper.destroy()
+  process.exit(0)
+})

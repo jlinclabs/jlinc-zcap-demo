6ab296ceff0019ce26cb9449b0e2776876881294
Jared Grippe <jared@deadlyicon.com>
2022-05-16 15:03:43 -0700

    WIP: removing replacing zcap with jlinx

--- http-server/index.js
+++ http-server/index.js
@@ -4,236 +4,243 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const hbs = require('express-hbs')
 const jwt = require('jsonwebtoken')
-const zcap = require('@jlinc/zcap')
 
 hbs.handlebars.registerHelper('toJSON', object =>
   new hbs.handlebars.SafeString(JSON.stringify(object), null, 2)
 )
 
-function createHttpServer(options){
-  const appName = options.name
+// appDirectory = Path.resolve(appDirectory)
+// if (!appDirectory) throw new Error(`app directory required`)
+// process.chdir(appDirectory)
+// const configPath = Path.resolve(appDirectory + '/config.json')
+// const config = require(configPath)
+// config.appDirectory = appDirectory
+// if (process.env.PORT) config.port = process.env.PORT
+// return createHttpServer(config)
 
-  const app = express()
-  app.options = options
-  app.port = options.port
-  app.url = options.url
-  app.pg = require('./postgresql')(options)
-  app.hl = require('./hyperlinc')(options)
-  app.users = require('./models/users')(app)
 
-  app.use(express.static(__dirname + '/public'));
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json({ }))
-  app.use(cookieParser())
-  app.engine('hbs', hbs.express4({
-    partialsDir: __dirname + '/views/partials',
-    defaultLayout: __dirname + '/views/layout/default.hbs',
-  }))
-  app.set('view engine', 'hbs')
-  app.set('views', __dirname + '/views')
-  Object.assign(app.locals, {
-    appName,
-    appColor: options.color,
-    // TODO move this static lists
-    partnerApps: options.partnerApps,
-  })
+const appName = options.name
 
-  const SESSION_SECRET = `dont tell anyone this is ${appName}`
-  const COOKIE_NAME = `session`
+const app = express()
+module.exports = app
 
-  app.start = async function start(){
-    await app.pg.connect()
-    await app.hl.connect()
-    await new Promise((resolve, reject) => {
-      app.server = app.listen(app.port, error => {
-        if (error) reject(error); else resolve();
-      })
-    })
-  }
+app.port = getEnvVar('PORT')
+app.storagePath = getEnvVar('JLINX_STORAGE')
 
-  // ROUTES
-  const router = Router()
-  app.use(router)
+app.port = options.port
+app.url = options.url
+app.pg = require('./postgresql')(options)
+app.hl = require('./hyperlinc')(options)
+app.users = require('./models/users')(app)
 
-  async function verifySession(sessionJwt){
-    const session = await new Promise((resolve, reject) => {
-      jwt.verify(sessionJwt, SESSION_SECRET, (error, session) => {
-        if (error) reject(error)
-        else resolve(session)
-      })
-    })
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({ }))
+app.use(cookieParser())
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials',
+  defaultLayout: __dirname + '/views/layout/default.hbs',
+}))
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+Object.assign(app.locals, {
+  appName,
+  appColor: options.color,
+  // TODO move this static lists
+  partnerApps: options.partnerApps,
+})
 
-    return session
-  }
+const SESSION_SECRET = `dont tell anyone this is ${appName}`
+const COOKIE_NAME = `session`
 
-  router.use('*', async (req, res, next) => {
-    const sessionJwt = req.cookies[COOKIE_NAME]
-    let session
-    if (sessionJwt){
-      try{
-        session = await verifySession(sessionJwt)
-      }catch(error){
-        res.cookie('session', null)
-      }
-    }
-
-    let currentUser
-    if (session) {
-      currentUser = await app.users.get(session.username)
-      if (!currentUser){
-        res.status(401).clearCookie(COOKIE_NAME).redirect('/')
-        return
-      }
-    }
-
-    Object.assign(res.locals, { session, currentUser })
-
-    next()
-  })
-
-  router.get('/', async (req, res) => {
-    res.render('index', {
-      users: await app.users.getAll(),
-      hyperlincStatus: await app.hl.status(),
+app.start = async function start(){
+  await app.pg.connect()
+  await app.hl.connect()
+  await new Promise((resolve, reject) => {
+    app.server = app.listen(app.port, error => {
+      if (error) reject(error); else resolve();
     })
   })
-
-  router.get('/login', async (req, res) => {
-    if (req.query.zcap){
-      const delegatedCapability = zcap.verifyZcapInvocation(req.query.zcap);
-      const jlincDid = delegatedCapability.zcap.invokerDid
-      const user = await app.users.findByJlincDid(jlincDid)
-      return user
-        ? createSessionCookie(res, user.username)
-        : res.render('zcapsignup', { jlincDid })
-    }else{
-      res.render('login')
-    }
-  })
-
-  function createSessionCookie(res, username, destination = '/'){
-    if (!username) throw new Error('no username!')
-    res
-      .cookie(COOKIE_NAME, jwt.sign(
-        { username },
-        SESSION_SECRET,
-        { expiresIn: 86400 }
-      ))
-      .redirect(destination)
-  }
-
-  router.post('/login', async (req, res) => {
-    const { username } = req.body
-    const user = await app.users.get(username)
-    if (user) {
-      createSessionCookie(res, user.username)
-    }else{
-      res
-        .clearCookie(COOKIE_NAME)
-        .render('error', {
-          error: { message: `user "${username}" not found` },
-        })
-    }
-  })
-
-  router.post('/logout', (req, res) => {
-    res.status(200).clearCookie(COOKIE_NAME).redirect('/')
-  })
-
-  router.get('/signup', (req, res) => {
-    res.render('signup')
-  })
-
-  router.post('/signup', async (req, res) => {
-    const { username, realname } = req.body
-    const user = await app.users.create({ username, realname })
-    createSessionCookie(res, user.username, `/@${user.username}`)
-  })
-
-  router.get('/@:username', async (req, res) => {
-    const { currentUser } = res.locals
-    const { username } = req.params
-    const itsUs = currentUser && currentUser.username === username
-    const user = itsUs ? currentUser : (await app.users.get(username))
-    const hyperlincEvents = user && await app.users._getAllHyperlincEvents(username)
-    res.render('profile', {
-      username, itsUs, user, hyperlincEvents
-    })
-  })
-
-  router.post('/profile', async (req, res) => {
-    const { username } = res.locals.currentUser
-    const changes = req.body
-    await app.users.updateProfile(username, changes)
-    res.redirect(`/@${username}`)
-  })
-
-  router.get('/send-me-to', (req, res) => {
-    const { currentUser } = res.locals
-    const appUrl = req.query.app
-    res.redirect(`${appUrl}/hyper-login?hlid=${currentUser.hyperlinc_id}`)
-  })
-
-  router.get('/hyper-login', async (req, res) => {
-    const hyperlincId = req.query.hlid
-    const user = await app.users.findByHyperlincId(hyperlincId)
-    if (user) return createSessionCookie(res, user.username)
-
-    const [hlProfile] = await app.hl.getProfiles([hyperlincId])
-    if (hlProfile){
-      if (hlProfile.preferredUsername){
-        const user = await app.users.create({
-          username: hlProfile.preferredUsername,
-          hyperlincId,
-        })
-        return createSessionCookie(res, user.username)
-      }
-      const hyperlincStatus = await app.hl.status()
-      return res.render('hypersignup', {
-        hyperlincId, hlProfile, hyperlincStatus
-      })
-    }
-    res.render('error', { error: { message: 'didnt work :(' }})
-  })
-
-  router.post('/hyper-signup', async (req, res) => {
-    const { hlid: hyperlincId, username } = req.body
-    const user = await app.users.create({ username, hyperlincId })
-    return createSessionCookie(res, user.username)
-  })
-
-  router.post('/zcap-signup', async (req, res) => {
-    const { jlincDid, username } = req.body
-    const user = await app.users.create({ username, jlincDid })
-    return createSessionCookie(res, user.username)
-  })
-
-  router.get('/__hyperlinc/:id', async (req, res) => {
-    const { id } = req.params
-    const identity = await app.hl.getIdentity(id)
-    await identity.update()
-    const hyperlincEvents = await identity.getAllEvents()
-    const writable = (
-      await app.pg.query(
-        `
-        SELECT 1
-        FROM hyperlinc_secret_keys
-        WHERE hyperlinc_id = $1
-        `,
-        [id]
-      )
-    ).rowCount > 0
-    res.render('hyperlinc_events', {
-      writable,
-      hyperlincEvents,
-    })
-  })
-
-  router.use((error, req, res, next) => {
-    res.render('error', { error })
-  })
-
-  return app
 }
 
-module.exports = createHttpServer
+// ROUTES
+const router = Router()
+app.use(router)
+
+async function verifySession(sessionJwt){
+  const session = await new Promise((resolve, reject) => {
+    jwt.verify(sessionJwt, SESSION_SECRET, (error, session) => {
+      if (error) reject(error)
+      else resolve(session)
+    })
+  })
+
+  return session
+}
+
+router.use('*', async (req, res, next) => {
+  const sessionJwt = req.cookies[COOKIE_NAME]
+  let session
+  if (sessionJwt){
+    try{
+      session = await verifySession(sessionJwt)
+    }catch(error){
+      res.cookie('session', null)
+    }
+  }
+
+  let currentUser
+  if (session) {
+    currentUser = await app.users.get(session.username)
+    if (!currentUser){
+      res.status(401).clearCookie(COOKIE_NAME).redirect('/')
+      return
+    }
+  }
+
+  Object.assign(res.locals, { session, currentUser })
+
+  next()
+})
+
+router.get('/', async (req, res) => {
+  res.render('index', {
+    users: await app.users.getAll(),
+    hyperlincStatus: await app.hl.status(),
+  })
+})
+
+router.get('/login', async (req, res) => {
+  if (req.query.zcap){
+    const delegatedCapability = zcap.verifyZcapInvocation(req.query.zcap);
+    const jlincDid = delegatedCapability.zcap.invokerDid
+    const user = await app.users.findByJlincDid(jlincDid)
+    return user
+      ? createSessionCookie(res, user.username)
+      : res.render('zcapsignup', { jlincDid })
+  }else{
+    res.render('login')
+  }
+})
+
+function createSessionCookie(res, username, destination = '/'){
+  if (!username) throw new Error('no username!')
+  res
+    .cookie(COOKIE_NAME, jwt.sign(
+      { username },
+      SESSION_SECRET,
+      { expiresIn: 86400 }
+    ))
+    .redirect(destination)
+}
+
+router.post('/login', async (req, res) => {
+  const { username } = req.body
+  const user = await app.users.get(username)
+  if (user) {
+    createSessionCookie(res, user.username)
+  }else{
+    res
+      .clearCookie(COOKIE_NAME)
+      .render('error', {
+        error: { message: `user "${username}" not found` },
+      })
+  }
+})
+
+router.post('/logout', (req, res) => {
+  res.status(200).clearCookie(COOKIE_NAME).redirect('/')
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+router.post('/signup', async (req, res) => {
+  const { username, realname } = req.body
+  const user = await app.users.create({ username, realname })
+  createSessionCookie(res, user.username, `/@${user.username}`)
+})
+
+router.get('/@:username', async (req, res) => {
+  const { currentUser } = res.locals
+  const { username } = req.params
+  const itsUs = currentUser && currentUser.username === username
+  const user = itsUs ? currentUser : (await app.users.get(username))
+  const hyperlincEvents = user && await app.users._getAllHyperlincEvents(username)
+  res.render('profile', {
+    username, itsUs, user, hyperlincEvents
+  })
+})
+
+router.post('/profile', async (req, res) => {
+  const { username } = res.locals.currentUser
+  const changes = req.body
+  await app.users.updateProfile(username, changes)
+  res.redirect(`/@${username}`)
+})
+
+router.get('/send-me-to', (req, res) => {
+  const { currentUser } = res.locals
+  const appUrl = req.query.app
+  res.redirect(`${appUrl}/hyper-login?hlid=${currentUser.hyperlinc_id}`)
+})
+
+router.get('/hyper-login', async (req, res) => {
+  const hyperlincId = req.query.hlid
+  const user = await app.users.findByHyperlincId(hyperlincId)
+  if (user) return createSessionCookie(res, user.username)
+
+  const [hlProfile] = await app.hl.getProfiles([hyperlincId])
+  if (hlProfile){
+    if (hlProfile.preferredUsername){
+      const user = await app.users.create({
+        username: hlProfile.preferredUsername,
+        hyperlincId,
+      })
+      return createSessionCookie(res, user.username)
+    }
+    const hyperlincStatus = await app.hl.status()
+    return res.render('hypersignup', {
+      hyperlincId, hlProfile, hyperlincStatus
+    })
+  }
+  res.render('error', { error: { message: 'didnt work :(' }})
+})
+
+router.post('/hyper-signup', async (req, res) => {
+  const { hlid: hyperlincId, username } = req.body
+  const user = await app.users.create({ username, hyperlincId })
+  return createSessionCookie(res, user.username)
+})
+
+router.post('/zcap-signup', async (req, res) => {
+  const { jlincDid, username } = req.body
+  const user = await app.users.create({ username, jlincDid })
+  return createSessionCookie(res, user.username)
+})
+
+router.get('/__hyperlinc/:id', async (req, res) => {
+  const { id } = req.params
+  const identity = await app.hl.getIdentity(id)
+  await identity.update()
+  const hyperlincEvents = await identity.getAllEvents()
+  const writable = (
+    await app.pg.query(
+      `
+      SELECT 1
+      FROM hyperlinc_secret_keys
+      WHERE hyperlinc_id = $1
+      `,
+      [id]
+    )
+  ).rowCount > 0
+  res.render('hyperlinc_events', {
+    writable,
+    hyperlincEvents,
+  })
+})
+
+router.use((error, req, res, next) => {
+  res.render('error', { error })
+})
